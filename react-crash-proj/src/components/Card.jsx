2b1b82fbad2c56579e9d4db0e26d87e85fa60f56
Izuuii <denverdeleon22@gmail.com>
2025-04-05 19:14:33 +0800

    Added Crud Functionalities

--- react-crash-proj/src/components/Card.jsx
+++ react-crash-proj/src/components/Card.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// Children is a built-in prop that represents the inner content between opening and closing tags of a component.
+// - Lets components accept and render nested elements.
 const Card = ({ children, bg = 'bg-gray-100' }) => {
     return (
         <div className={`${bg} p-6 rounded-lg shadow-md`}>
--- react-crash-proj/src/pages/HomePage.jsx
+++ react-crash-proj/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import HomeCards from '../components/HomeCards'
-import JobListing from '../components/JobListing'
+
 import JobListings from '../components/JobListings'
 import ViewAllJobs from '../components/ViewAllJobs'
 
@@ -10,7 +10,7 @@ const HomePage = () => {
         <>
             <Hero />
             <HomeCards/>
-            <JobListings/>
+            <JobListings isHome={true}/> 
             <ViewAllJobs/>
         </>
     )
